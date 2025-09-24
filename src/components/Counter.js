b406bb281134791eca09dbb816dde50d76347e64
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-09-24 14:33:38 -0700

    feat: update Counter so it uses useSelector and counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,36 +1,15 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
 
-  const incrementHandler = () => {
-    dispatch({ type: "increment" });
-  };
-
-  const decrementHandler = () => {
-    dispatch({ type: "decrement" });
-  };
-
-  const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
-  };
-
-  const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
-  };
+  const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {show && <div className={classes.value}>{counter}</div>}
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increase by 5</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
