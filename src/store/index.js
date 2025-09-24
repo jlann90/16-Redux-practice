6d0c84de220bb7a868a3d5b21b126e519560ba15
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-09-24 12:22:39 -0700

    feat: create store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,17 @@
 import { createStore } from "redux";
 
-const counterReducer = createStore();
-const store = createStore();
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
