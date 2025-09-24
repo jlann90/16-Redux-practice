bc35a49488ab5bebed9169e7a64649cf25782001
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-09-24 12:26:51 -0700

    feat: update index.jsx to add Redux Provider and store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
-import counterReducer from "./store/counter";
-
-const store = createStore(counterReducer);
+import store from "./store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
